237057ab967e720ce81961cd68691266168b3ebb
Dustin Brunner <dustinbrunner@Dustins-Air.mshome.net>
2022-11-23 10:43:05 +0100

    add gap and random jitter when cutting patches

--- src/dataset.py
+++ src/dataset.py
@@ -1,7 +1,8 @@
+import numpy as np
 import torch
 from torchvision import transforms
 import torchvision.transforms.functional as TF
-from torchvision.transforms import Compose, RandomResizedCrop, RandomGrayscale, RandomHorizontalFlip, GaussianBlur, RandomSolarize, ColorJitter
+from torchvision.transforms import Compose, RandomResizedCrop, RandomGrayscale, RandomHorizontalFlip, GaussianBlur, ColorJitter, RandomSolarize, ToPILImage, ToTensor, RandomCrop, CenterCrop, Resize
 
 
 # transform that will be applied to every raw image
@@ -23,20 +24,40 @@ RELIC_AUGMENTATIONS = [
 ]
 
 
+def load_tiny_imagenet(tiny_imagenet_folder, dataset_type='train', transform=transforms.ToPILImage()):
+    images = []
+    if dataset_type == 'train':
+        for foldername in tqdm(os.listdir(os.path.join(tiny_imagenet_folder, dataset_type))):
+            for filename in os.listdir(os.path.join(tiny_imagenet_folder, dataset_type, foldername, "images")):
+                img = cv2.imread(os.path.join(tiny_imagenet_folder, dataset_type, foldername, "images", filename))
+                if img is not None:
+                    images.append(transform(img) if transform else img)
+    else:
+        # 'val' or 'test'
+        for filename in tqdm(os.listdir(os.path.join(tiny_imagenet_folder, dataset_type, "images"))):
+            img = cv2.imread(os.path.join(tiny_imagenet_folder, dataset_type, "images", filename))
+            if img is not None:
+                images.append(transform(img) if transform else img)
+
+    return images
+
+
 def image_to_patches(img):
     """Crop split_per_side x split_per_side patches from input image.
     Args:
-        img (PIL Image): input image.
+        img (Tensor image): input image.
     Returns:
-        list[PIL Image]: A list of cropped patches.
+        list[Tensor image]: A list of cropped patches.
     """
     splits_per_side = 3  # split of patches per image side
-    h, w = img.size()[1:]
-    h_grid = h // splits_per_side
-    w_grid = w // splits_per_side
+    img_size = img.size()[-1]
+    grid_size = img_size // splits_per_side
+    patch_size = img_size // 4
 
+    # we first use a center crop (to ensure gap) followed by a random crop (jitter) followed by resize (to ensure img_size stays the same)
+    random_jitter = Compose([CenterCrop(grid_size - patch_size // 4), RandomCrop(patch_size), Resize(img_size)])
     patches = [
-        TF.crop(img, i * h_grid, j * w_grid, h_grid, w_grid)
+        random_jitter(TF.crop(img, i * grid_size, j * grid_size, grid_size, grid_size))
         for i in range(splits_per_side)
         for j in range(splits_per_side)
     ]
@@ -50,29 +71,38 @@ class OriginalPatchLocalizationDataset(torch.utils.data.Dataset):
     A sample is made up of the 8 possible tasks for a given grid ((center, neighbor), labels)
     """
 
-    def __init__(self, data_source, transform=None):
+    def __init__(self, data_source, transform=None, samples_per_image=8):
         self.data_source = data_source
         self.transform = transform if transform else TINY_IMAGENET_TRANSFORM
+        self.samples_per_image = samples_per_image
 
     def __len__(self):
         return len(self.data_source)
 
     def __getitem__(self, idx):
+        # load image from data_source
         img = self.data_source[idx]
-
+        # apply transform
         img = self.transform(img)
-        # split image into 9 patches
-        patches = image_to_patches(img)
-        # generate the permutations between the patches
-        perms = self.generate_perms(patches)
-        # labels for patch pairs
-        patch_labels = torch.LongTensor(list(range(8)))
+        # get samples_per_image samples from img
+        samples = self.image_to_samples(img)
 
-        samples = (torch.stack(perms), patch_labels)
         return samples
 
-    def generate_perms(self, patches):
-        return [torch.cat((patches[4], patches[i]), dim=0) for i in range(9) if i != 4]
+    def image_to_samples(self, img):
+        # convert image into patches
+        patches = image_to_patches(img)
+        # randomly select n_samples from all possible labels without replacement
+        labels = np.random.choice(8, self.samples_per_image)
+
+        samples = []
+        for label in labels:
+            if label >= 4:
+                # the middle patch (number 4) is never a neighbor
+                label += 1
+            samples.append(torch.stack((patches[4], patches[label])))
+
+        return torch.stack(samples), torch.from_numpy(labels)
 
 
 class OurPatchLocalizationDataset(OriginalPatchLocalizationDataset):
@@ -81,14 +111,22 @@ class OurPatchLocalizationDataset(OriginalPatchLocalizationDataset):
     A sample is made up of the 8 possible tasks for a given grid ((center, random_aug(neighbor), random_aug(neighbor)), labels)
     """
 
-    def __init__(self, data_source, transform=None, aug_transforms=None):
-        super(OurPatchLocalizationDataset, self).__init__(data_source, transform)
+    def __init__(self, data_source, transform=None, samples_per_image=8, aug_transforms=None):
+        super(OurPatchLocalizationDataset, self).__init__(data_source, transform, samples_per_image)
 
         self.aug_transform = Compose(aug_transforms) if aug_transforms else Compose(RELIC_AUGMENTATIONS)
 
-    def generate_perms(self, patches):
-        # randomly transform outer patch in two different ways
-        return [
-            torch.cat((TF.resize(patches[4], 224), self.aug_transform(patches[i]), self.aug_transform(patches[i])), dim=0)
-            for i in range(9) if i != 4
-        ]
+    def image_to_samples(self, img):
+        # convert image into patches
+        patches = image_to_patches(img)
+        # randomly select n_samples from all possible labels without replacement
+        labels = np.random.choice(8, self.samples_per_image)
+
+        samples = []
+        for label in labels:
+            if label >= 4:
+                # the middle patch (number 4) is never a neighbor
+                label += 1
+            samples.append(torch.stack((patches[4], self.aug_transform(patches[label]), self.aug_transform(patches[label]))))
+
+        return torch.stack(samples), torch.from_numpy(labels)
