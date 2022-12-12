2f531a292c70ed20c639a4ada06e97bb3ee8042b
brunnedu <dustin.brunner@gmx.ch>
2022-12-12 19:17:06 +0100

    deepcopy run_params in create_optuna_objective

--- src/optuna.py
+++ src/optuna.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from datetime import datetime
 from typing import Callable
 
@@ -12,6 +13,7 @@ def create_optuna_objective(
     """
 
     experiment_id = RUN_PARAMS["experiment_id"]
+    run_params_copy = deepcopy(RUN_PARAMS)
 
     def objective(trial):
 
@@ -19,38 +21,35 @@ def create_optuna_objective(
         for kwarg, val in RUN_PARAMS.items():
             if not (isinstance(val, tuple) or kwarg == "optimizer_kwargs"):
                 continue
-            print(kwarg)
             if kwarg == "optimizer_kwargs":
-                print("optimkwarg")
                 for optim_kwarg, optim_val in RUN_PARAMS["optimizer_kwargs"].items():
                     if isinstance(optim_val, tuple):
                         t, lo, hi = optim_val
                         # optimizer_kwargs are always float
-                        RUN_PARAMS["optimizer_kwargs"][optim_kwarg] = trial.suggest_float(optim_kwarg, lo, hi, log=('log' in t))
+                        run_params_copy["optimizer_kwargs"][optim_kwarg] = trial.suggest_float(optim_kwarg, lo, hi, log=('log' in t))
 
             else:
                 # type, lower_bound, upper_bound
                 t, lo, hi = val
-
                 if 'int' in t:
                     if 'log' in t:
-                        RUN_PARAMS[kwarg] = 2 ** trial.suggest_int(kwarg, lo, hi, log=False)
+                        run_params_copy[kwarg] = 2 ** trial.suggest_int(kwarg, lo, hi, log=False)
                     else:
-                        RUN_PARAMS[kwarg] = trial.suggest_int(kwarg, lo, hi, log=False)
+                        run_params_copy[kwarg] = trial.suggest_int(kwarg, lo, hi, log=False)
                 elif 'float' in t:
-                    RUN_PARAMS[kwarg] = trial.suggest_float(kwarg, lo, hi, log=('log' in t))
+                    run_params_copy[kwarg] = trial.suggest_float(kwarg, lo, hi, log=('log' in t))
                 else:
                     raise ValueError(f'Type of keyword argument `{kwarg}` must be either "int" or "float".')
 
         # modify experiment id to have unique id for each experiment run during optuna search
-        current_time = datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
-        RUN_PARAMS["experiment_id"] = experiment_id + "_" + current_time
+        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        run_params_copy["experiment_id"] = experiment_id + "_" + current_time
 
         # run trial
-        if "pretext_model" in RUN_PARAMS:
-            best_acc = run_downstream(**RUN_PARAMS)
+        if "pretext_model" in run_params_copy:
+            best_acc = run_downstream(**run_params_copy)
         else:
-            best_acc = run_pretext(**RUN_PARAMS)
+            best_acc = run_pretext(**run_params_copy)
 
         return best_acc
 
