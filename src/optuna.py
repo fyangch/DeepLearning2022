3e4f8495e1a83a9e4dae1918788bb1522e9feec7
brunnedu <dustin.brunner@gmx.ch>
2022-12-13 11:51:47 +0100

    clean up optuna code

--- src/optuna.py
+++ src/optuna.py
@@ -1,56 +1,58 @@
 from copy import deepcopy
 from datetime import datetime
-from typing import Callable
+from typing import Callable, Union
 
 from src.train import run_downstream, run_pretext
 
 
+def get_param_val(kwarg, val, trial) -> Union[float, int]:
+
+    # type has to contain either "int" or "float" and can optionally contain "log" for logarithmic sampling
+    t, lo, hi = val
+
+    if "float" in t:
+        return trial.suggest_float(kwarg, lo, hi, log=("log" in t))
+    elif "int" in t:
+        suggested_int = trial.suggest_int(kwarg, lo, hi, log=False)
+        return suggested_int if "log" not in t else 2 ** suggested_int
+    else:
+        raise ValueError(f"The type of parameter to search over should be 'int' or 'float'. Type provided: {t}")
+
+
 def create_optuna_objective(
-        RUN_PARAMS: dict,
+        run_params: dict,
 ) -> Callable:
     """
     Create the objective function used for the optuna hyperparameter search.
     """
 
-    experiment_id = RUN_PARAMS["experiment_id"]
-    run_params_copy = deepcopy(RUN_PARAMS)
+    experiment_id = run_params["experiment_id"]
+    current_run_params = deepcopy(run_params)
 
     def objective(trial):
 
-        # add trial kwargs to RUN_DOWNSTREAM_PARAMS
-        for kwarg, val in RUN_PARAMS.items():
+        # fill current_run_params with sampled parameter values (bayesian optimization)
+        for kwarg, val in run_params.items():
             if not (isinstance(val, tuple) or kwarg == "optimizer_kwargs"):
                 continue
             if kwarg == "optimizer_kwargs":
-                for optim_kwarg, optim_val in RUN_PARAMS["optimizer_kwargs"].items():
+                for optim_kwarg, optim_val in run_params["optimizer_kwargs"].items():
                     if isinstance(optim_val, tuple):
-                        t, lo, hi = optim_val
-                        # optimizer_kwargs are always float
-                        run_params_copy["optimizer_kwargs"][optim_kwarg] = trial.suggest_float(optim_kwarg, lo, hi, log=('log' in t))
-
+                        current_run_params["optimizer_kwargs"][optim_kwarg] = get_param_val(optim_kwarg, optim_val, trial)
             else:
-                # type, lower_bound, upper_bound
-                t, lo, hi = val
-                if 'int' in t:
-                    if 'log' in t:
-                        run_params_copy[kwarg] = 2 ** trial.suggest_int(kwarg, lo, hi, log=False)
-                    else:
-                        run_params_copy[kwarg] = trial.suggest_int(kwarg, lo, hi, log=False)
-                elif 'float' in t:
-                    run_params_copy[kwarg] = trial.suggest_float(kwarg, lo, hi, log=('log' in t))
-                else:
-                    raise ValueError(f'Type of keyword argument `{kwarg}` must be either "int" or "float".')
+                current_run_params[kwarg] = get_param_val(kwarg, val, trial)
 
         # modify experiment id to have unique id for each experiment run during optuna search
         current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        run_params_copy["experiment_id"] = experiment_id + "_" + current_time
+        current_run_params["experiment_id"] = experiment_id + "_" + current_time
 
         # run trial
-        if "pretext_model" in run_params_copy:
-            best_acc = run_downstream(**run_params_copy)
+        if "pretext_model" in current_run_params:
+            best_acc = run_downstream(**current_run_params)
         else:
-            best_acc = run_pretext(**run_params_copy)
+            best_acc = run_pretext(**current_run_params)
 
+        # return the accuracy this parameter combination achieved
         return best_acc
 
     return objective
